474b3858e544d5b6120d1fdec2213ba7b74d88bf
guogaoqin <67480337@qq.com>
2020-04-08 16:02:26 +0800

    JDK1.8源码

---
+++ src/com/carroll/test/arraylist/test.java
@@ -0,0 +1,15 @@
+package com.carroll.test.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Auther Carroll
+ * @Date 2020/4/8
+ * @e-mail ggq_carroll@163.com
+ */
+public class test {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+    }
+}
